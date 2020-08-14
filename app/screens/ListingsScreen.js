6dcbc0882d9e6860b752f6c87057f96c6ed0a1d0
Adomas Domeika <adomas.domeika@jet2.com>
2020-08-14 16:47:55 +0100

    Done input components

---
+++ app/screens/ListingsScreen.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+import Screen from '../components/Screen';
+import Card from '../components/Card';
+import colors from '../config/colors';
+
+const listings = [
+  {
+    id: 1,
+    title: 'Red Jacket for Sale',
+    price: 100,
+    image: require('../assets/jacket.jpg'),
+  },
+  {
+    id: 2,
+    title: 'Other Jacket for Sale',
+    price: 90,
+    image: require('../assets/jacket.jpg'),
+  },
+];
+
+export default function ListingsScreen() {
+  return (
+    <Screen style={styles.screen}>
+      <FlatList
+        data={listings}
+        keyExtractor={(listing) => listing.id.toString()}
+        renderItem={({ item }) => (
+          <Card
+            title={item.title}
+            subTitle={'$' + item.price}
+            image={item.image}
+          />
+        )}
+      />
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 20,
+    backgroundColor: colors.light,
+  },
+});
---
+++ app/screens/MessagesScreen.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { StyleSheet, FlatList, View } from 'react-native';
+
+import ListItem from '../components/ListItem';
+import ListItemDeleteAction from '../components/ListItemDeleteAction';
+import ListItemSeparator from '../components/ListItemSeparator';
+import Screen from '../components/Screen';
+
+export default function MessagesScreen() {
+  const [messages, setMessages] = useState([
+    {
+      id: 1,
+      title: 'Title 1',
+      description: 'Description 1',
+      image: require('../assets/jacket.jpg'),
+    },
+    {
+      id: 2,
+      title: 'Title 2',
+      description: 'Description 2',
+      image: require('../assets/jacket.jpg'),
+    },
+  ]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const handleDelete = (message) => {
+    setMessages(messages.filter((m) => m.id !== message.id));
+  };
+
+  return (
+    <Screen>
+      <FlatList
+        data={messages}
+        keyExtractor={(message) => message.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+            onPress={() => console.log(item)}
+            renderRightActions={() => (
+              <ListItemDeleteAction onPress={() => handleDelete(item)} />
+            )}
+          />
+        )}
+        ItemSeparatorComponent={ListItemSeparator}
+        refreshing={refreshing}
+        onRefresh={() => {
+          setMessages([
+            {
+              id: 2,
+              title: 'Title 2',
+              description: 'Description 2',
+              image: require('../assets/jacket.jpg'),
+            },
+          ]);
+        }}
+      />
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({});
